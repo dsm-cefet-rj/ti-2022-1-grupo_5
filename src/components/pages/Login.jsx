3e914b6b44ae50713bda98d3ed53ceee6ffd63ab
Letícia Maia <61125269+leticiamaiat@users.noreply.github.com>
2022-06-15 11:59:26 -0300

    Validação dos formulários de Login/cadastro

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from  "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import "./lan.css";
 
 
@@ -17,40 +19,65 @@ export default function Login() {
     event.preventDefault();
   }
 
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email: </Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Senha: </Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <div class="grid-container">
-           <div class="grid-item">
-          <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
-          <Link to='/' className="Login">Login</Link>
-        </Button>     
-        </div>
-        <div class="grid-item">
-          <Button block="true" size="lg" type="submit">
-           <Link to='/Register' className="Login">Criar Conta</Link>
-        </Button></div>
-        </div>
-          
-    </Form>
+  const formik = useFormik({
+    
+    initialValues: {
+      email: '',
+      senha: '',
+    },
+    onSubmit: function (values) {
+      alert(`Voce entrou na sua conta! 
+      Nome: ${values.email}.
+      senha: ${values.senha}. 
+      } `);
+    },
 
+    validationSchema: Yup.object({
+      email: Yup.string()
+              .email("Email invalido")
+              .required(),
+      senha: Yup.string()
+            .min(6, " necessário no mínimo 6 dígitos")
+            .required("campo obrigatorio"),
+    
+  })})
+
+  
+
+  return (
+    <div className="Loginform">
+      <form onSubmit={formik.handleSubmit} className="loginForm">
+      <h1 className='Loginform' >Login</h1>
+        <div className='Loginfrom'>
+          <label for="email">Email:&nbsp;&nbsp; &nbsp;</label>
+          <input type="email" name="email" id="email"
+            className={`block w-full rounded border py-1 px-2${formik.touched.email && formik.errors.email}`}
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
+          {formik.touched.email && formik.errors.email && (
+            <span className='text-red-400'>{formik.errors.email}</span>
+          )}
+        </div>
+        
+        <div className='Login'>
+          <label for="senha">Senha:&nbsp;&nbsp;</label>
+          <input type="string" name="senha" id="senha"
+            className={`block w-full rounded border py-1 px-2${formik.touched.senha && formik.errors.senha ? 'border-red-400' : 'border-gray-300'}`}
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.senha} />
+          {formik.touched.senha && formik.errors.senha && (
+            <span className='text-red-400'>{formik.errors.senha}</span>
+          )}
+        </div>
+        <div class="grid-container">
+          <div class="grid-item">
+          <Button block="true" size="lg" type="submit" >
+          <Link to='/' text-align = "center" className="Login">Login</Link>
+          </Button>
+            
+          <div class="grid-item">
+          <Button block="true" size="lg" type="submit">
+           <Link to='/Register'  text-align = "center"className="Login">Criar Conta</Link>
+        </Button></div></div></div>
+      </form>
     </div>
   );
 }
--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,19 +1,15 @@
 import React, { useState } from  "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import "./lan.css";
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import "./lan.css";
 
 
-
-
-export default function Register() {
-  const [nome, setNome] = useState("")
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [endereço, setEndereco] = useState("")
-  const [cep, setCep] = useState("")
-  const [complemento, setComplemento] = useState("")
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -22,76 +18,92 @@ export default function Register() {
   function handleSubmit(event) {
     event.preventDefault();
   }
+  
+
+  const formik = useFormik({
+    
+    initialValues: {
+      nome: '',
+      email: '',
+      cep: '',
+      age: '',
+    },
+    onSubmit: function (values) {
+      alert(`Voce esta registrado! 
+      Nome: ${values.name}.
+      Email: ${values.email}. 
+      Idade: ${values.age} 
+      Cep: ${values.cep} `);
+    },
+
+    validationSchema: Yup.object({
+      name: Yup.string()
+              .label('Full Name')
+              .matches(/^[A-Za-z ]*$/, 'Insira um nome Válido')
+      .max(40)
+              .required(),
+      email: Yup.string()
+              .email('Email invalido')
+              .required(),
+      cep: Yup.string()
+      .matches(/^[0-9]+$/, "Must be only digits")
+      .min(8, 'Deve conter 8 dígitos')
+      .max(8, 'Deve conter 8 dígitos')
+            .required(),
+      age: Yup.number()
+            .min(18, 'Precisa ser maior de 18 anos.')
+            .max(99, "Idade não permitida")
+            .required()
+    
+  })})
+
+  
 
   return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-      <Form.Group size="lg" controlId="Nome">
-          <Form.Label>Nome: </Form.Label>
-          <Form.Control
-            autoFocus
-            type="nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email: </Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="cep">
-          <Form.Label>CEP: </Form.Label>
-          <Form.Control
-            autoFocus
-            type="cep"
-            value={cep}
-            onChange={(e) => setCep(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="endereço">
-          <Form.Label>Endereço: </Form.Label>
-          <Form.Control
-            autoFocus
-            type="endereço"
-            value={endereço}
-            onChange={(e) => setEndereco(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="complemento">
-          <Form.Label>Complemento: </Form.Label>
-          <Form.Control
-            autoFocus
-            type="complemento"
-            value={complemento}
-            onChange={(e) => setComplemento(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Senha: </Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <div class="grid-container">
-           <div class="grid-item">
-          <Button block="true" size="lg" type="submit">
-          <Link to='/Login' className="Login">Já possuo conta</Link>
-        </Button>     
+    <div className="Loginform">
+      <form onSubmit={formik.handleSubmit} className="loginform">
+      <h1 className='Loginform'>Cadastro</h1>
+        <div className='Login'>
+          <label for="name">Nome Completo: &nbsp;&nbsp;</label>
+          <input type="text" name="name" id="name" 
+            className={`block w-full rounded border py-1 px-2 ${formik.touched.name && formik.errors.name ? 'border-red-400' : 'border-gray-300'}`}
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} />
+          {formik.touched.name && formik.errors.name && (
+            <span className='text-red-400'>{formik.errors.name}</span>
+          )}
         </div>
-        <div class="grid-item">
-          <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
-           <Link to='/Register' className="Login">Criar Conta</Link>
-        </Button></div>
+        <div className='Login'>
+          <label for="email">Email:&nbsp;&nbsp; </label>
+          <input type="email" name="email" id="email"
+            className={`block w-full rounded border py-1 px-2 ${formik.touched.email && formik.errors.email ? 'border-red-400' : 'border-gray-300'}`}
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
+          {formik.touched.email && formik.errors.email && (
+            <span className='text-red-400'>{formik.errors.email}</span>
+          )}
         </div>
-          
-    </Form>
+        <div className='Login'>
+          <label for="cep">CEP:&nbsp;&nbsp;</label>
+          <input type="number" name="cep" id="cep"
+            className={`block w-full rounded border py-1 px-2 ${formik.touched.cep && formik.errors.cep ? 'border-red-400' : 'border-gray-300'}`}
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.cep} />
+          {formik.touched.cep && formik.errors.cep && (
+            <span className='text-red-400'>{formik.errors.cep}</span>
+          )}
+        </div>
+        
+        <div className='Login'>
+          <label for="age">Idade: &nbsp;&nbsp;</label>
+          <input type="number" name="age" id="age"
+            className={`block w-full rounded border py-1 px-2 ${formik.touched.age && formik.errors.age}`}
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.age} />
+          {formik.touched.age && formik.errors.age && (
+            <span className='text-red-400'>{formik.errors.age}</span>
+          )}
+        </div>
+      
+          <Button className='grid-item' text-align = "center" type='submit'>Criar Conta</Button>
+       
+      </form>
     </div>
   );
 }
