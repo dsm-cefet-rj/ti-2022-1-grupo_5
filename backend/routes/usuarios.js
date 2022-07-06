3430065e67f5f1d384973b526c81159f270ffa8b
Renan Carvalho <renancps77@gmail.com>
2022-07-05 23:10:06 -0300

    Alteração no usuarios.js

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -7,22 +7,22 @@ router.post('/', async (req, res, next) => {
     res.status(201).send('Usuário foi cadastrado');
 });
 
-app.post("/auth/login", async (req, res) => {
+router.post("/auth/login", async (req, res) => {
     const {email, senha} = req.body
+    //checa se usuario existe
+    const user = await Usuario.findOne({email: email})
+    
+    if(!user){
+        return res.status(404).json({msg : "Usuário não encontrado!"})
+    }
+    
+    //checa se a senha esta correta
+    const checkPassword = await compare(senha, user.senha)
+    if(!checkPassword){
+        return res.status(422).json({msg : "Senha invalida"})
+    }
   })
   
-//checa se usuario existe
-const user = await Usuario.findOne({email: email})
-
-if(!user){
-    return res.status(404).json({msg : "Usuário não encontrado!"})
-}
-
-//checa se a senha esta correta
-const checkPassword = await compare(senha, user.senha)
-if(!checkPassword){
-    return res.status(422).json({msg : "Senha invalida"})
-}
 
 module.exports = router;
 
