402d1b6754fa3ca8cd87d14c0887cf2283899c7a
Letícia Maia <61125269+leticiamaiat@users.noreply.github.com>
2022-07-05 22:50:41 -0300

    usu

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -7,4 +7,26 @@ router.post('/', async (req, res, next) => {
     res.status(201).send('Usuário foi cadastrado');
 });
 
+app.post("/auth/login", async (req, res) => {
+    const {email, senha} = req.body
+  })
+  
+//checa se usuario existe
+const user = await Usuario.findOne({email: email})
+
+if(!user){
+    return res.status(404).json({msg : "Usuário não encontrado!"})
+}
+
+//checa se a senha esta correta
+const checkPassword = await compare(senha, user.senha)
+if(!checkPassword){
+    return res.status(422).json({msg : "Senha invalida"})
+}
+
 module.exports = router;
+
+//const userExists = await username.findOne({email: email})
+//if(userExists){
+//  return console.log("Utilize outro email")
+//}
